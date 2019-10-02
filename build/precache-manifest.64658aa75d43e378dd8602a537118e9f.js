e89f6418d910e87c82b20e985702119f4d760216
ajay jha <akjha@Tekions-MacBook-Pro-2.local>
2019-10-02 15:47:53 +0530

    done

---
+++ build/precache-manifest.64658aa75d43e378dd8602a537118e9f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243784739cfd62298fadf88d39d0e709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7a4c64c3373bd12c975",
+    "url": "/static/css/2.ac2e8ba5.chunk.css"
+  },
+  {
+    "revision": "f2b7c96ff2a991058f9d",
+    "url": "/static/css/main.df7df8d2.chunk.css"
+  },
+  {
+    "revision": "b7a4c64c3373bd12c975",
+    "url": "/static/js/2.307ed21b.chunk.js"
+  },
+  {
+    "revision": "f2b7c96ff2a991058f9d",
+    "url": "/static/js/main.1cc62f70.chunk.js"
+  },
+  {
+    "revision": "0695feffe46a21f9bd69",
+    "url": "/static/js/runtime~main.5fbbe860.js"
+  },
+  {
+    "revision": "7d312a73fa4c58a3b770eb9b4a2a0307",
+    "url": "/static/media/BJS.7d312a73.png"
+  },
+  {
+    "revision": "324065cfe95a75868d26433ad0a5c3a4",
+    "url": "/static/media/enhance-logo.324065cf.jpg"
+  },
+  {
+    "revision": "f77aba2d005e475d73de5b9b315407fc",
+    "url": "/static/media/finastra-logo-1200x630_0.f77aba2d.png"
+  },
+  {
+    "revision": "5a434db281fea1e23ae3ab5d482b9a68",
+    "url": "/static/media/quality_candidate.5a434db2.jpg"
+  },
+  {
+    "revision": "36dd1a230ea6eb1fcf36cc37b9177d30",
+    "url": "/static/media/skip_analysis.36dd1a23.jpg"
+  },
+  {
+    "revision": "fecc0a09c58cefb963126af429393615",
+    "url": "/static/media/talentpool.fecc0a09.jpg"
+  },
+  {
+    "revision": "8efd31aafb0fe79f413cab40cca42616",
+    "url": "/static/media/teamware.8efd31aa.png"
+  },
+  {
+    "revision": "506b14d0627ff9c775be210f4042b274",
+    "url": "/static/media/vyomlabs.506b14d0.png"
+  }
+]);
